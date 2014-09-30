3b7657d857bd1529385c422595a95c186144932e
James Denisco <james.denisco@genesys.com>
2014-09-30 16:55:15 -0400

    Initial checkin

---
+++ bartender.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+#   file: bartender
+#   date: 2014-09-29
+#   author: jdenisco
+#   email: james.denisco@genesys.com
+#
+# Copyright © 2014 jdenisco <james.denisco@genesys.com>
+#
+
+"""
+Description:
+"""
+
+questions = {
+    "strong": "Do ye like yer drinks strong?",
+    "salty": "Do ye like it with a salty tang?",
+    "bitter": "Are ye a lubber who likes it bitter?",
+    "sweet": "Would ye like a bit of sweetness with yer poison?",
+    "fruity": "Are ye one for a fruity finish?"
+}
+
+ingredients = {
+    "strong": ["glug of rum", "slug of whisky", "splash of gin"],
+    "salty": ["olive on a stick", "salt-dusted rim", "rasher of bacon"],
+    "bitter": ["shake of bitters", "splash of tonic", "twist of lemon peel"],
+    "sweet": ["sugar cube", "spoonful of honey", "spash of cola"],
+    "fruity": ["slice of orange", "dash of cassis", "cherry on top"]
+}
+
